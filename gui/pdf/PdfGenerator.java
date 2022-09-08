ef080e2086eb60833e60b1d17798bac7a4ad06cd
domcieplinski <ciedom93@gmail.com>
2022-09-08 13:11:27 +0200

    Last week learning about Maven. Working at *.pdf generator - rebuilded project to maven

--- gui/pdf/PdfGenerator.java
+++ gui/pdf/PdfGenerator.java
@@ -1,2 +1,29 @@
-package gui.pdf;public class PdfGenerator {
+package gui.pdf;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+
+import java.io.FileNotFoundException;
+
+public class PdfGenerator {
+    public static void main(String[] args){
+        try{
+            String path = "newssss.pdf";
+            PdfWriter pdfWriter = new PdfWriter(path);
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            pdfDocument.addNewPage();
+            Document document = new Document(pdfDocument);
+            Paragraph paragraph = new Paragraph("test");
+            document.add(paragraph);
+
+            document.close();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found!");
+        }
+
+
+    }
 }
