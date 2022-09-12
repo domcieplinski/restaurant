ae52e6c859c94826f363b0fa5c0cf879bcc46093
domcieplinski <ciedom93@gmail.com>
2022-09-12 14:25:56 +0200

    During application starting all settings are being checked whether they are already set or not.

--- gui/Main.java
+++ gui/Main.java
@@ -2,9 +2,28 @@ package gui;
 
 import gui.pdf.PdfGenerator;
 
+import java.util.prefs.Preferences;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        new MainView();
+        Preferences pref = Preferences.userNodeForPackage(Settings.class);
+        if(pref.get("title", "root") == "root" ||
+                pref.get("tables_amount", "root") == "root" ||
+                pref.get("tax", "root") == "root" ||
+                pref.get("street", "root") == "root" ||
+                pref.get("state", "root") == "root" ||
+                pref.get("zipcode", "root") == "root"
+        ){
+            showMessageDialog(null, "You have to set all settings!");
+            Settings settings = new Settings();
+            Settings.frame.setVisible(true);
+        }
+        else
+            new MainView();
     }
 
 
